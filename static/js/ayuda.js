a06ede1b506dbefca15808c4759704267a0d9595
1B0v0 <javierazapata@liceovvh.cl>
2025-08-14 14:24:04 -0400

    nuevo

---
+++ static/js/ayuda.js
@@ -0,0 +1,5 @@
+function toggleSidebar() {
+    const sidebar = document.getElementById("sidebar");
+    const isOpen = sidebar.style.left === "0px";
+    sidebar.style.left = isOpen ? "-250px" : "0px";
+}
